5a2f3b821d4e6a0b3700ef0ebcb5ab1008cb8a09
Xavi <xgxgxgc@gmail.com>
2020-10-29 08:52:04 +0100

    Añadiendo radio button y check box

---
+++ clients.py
@@ -0,0 +1,57 @@
+import var
+class Clientes():
+    def ValidarDNI(dni):
+        '''
+        codigo que controla si el DNI es correcto
+        :return:
+        '''
+        try:
+            tabla = 'TRWAGMYFPDXBNJZSQVHCKE'
+            dig_ext = 'XYZ'
+            reem_dig_xt = {'X': '0', 'Y': '1', 'Z': '2'}
+            numeros = '0123456789'
+            dni = dni.upper()
+            print(dni)
+            if len(dni) == 9:
+                dig_control = dni[8]
+                dni = dni[:8]
+                if dni[0] in dig_ext:
+                    dni = dni.replace(dni[0],reem_dig_xt[dni[0]])
+                return len(dni) == len([n for n in dni if n in numeros])  and tabla[int(dni) % 23] == dig_control
+            return False
+
+
+
+        except:
+            print('Error módulo validar DNI')
+            return None
+    def validoDni():
+        '''
+        Muestra mensaje de validación del DNI
+        :return:
+        '''
+        try:
+           dni = var.ui.EditDNI.text()
+           print(dni)
+           if Clientes.ValidarDni(dni):
+               var.ui.lblValidar.setStyleSheet('QLabel {color: green;}')
+               var.ui.lblValidar.setText('V')
+               var.ui.EditDNI.setText(dni.upper())
+           else:
+               var.ui.lblValidar.setStyleSheet('QLabel {color: red;}')
+               var.ui.lblValidar.setText('X')
+               var.ui.EditDNI.setText(dni.upper())
+
+        except:
+            print('Error módulo escribir DNI')
+            return None
+
+    def selSexo():
+        try:
+            if var.ui.rbtFem.isChecked():
+                print('Has elegido Femenino')
+            if  var.ui.rbtMas.isChecked():
+                print('Has elegido Masculino')
+
+        except Exception as error:
+            print('Error %s' % str(error))
